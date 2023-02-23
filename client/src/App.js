37171655cc1f45d62edaa23aea2a8c206568b1d6
bigdarrg <joebruce28@outlook.com>
2023-02-23 22:58:22 +0000

    Adding client folder.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "../src/App.css"
+
+function App() {
+  return (
+    <div>
+        This is a just a placeholder...
+    </div>
+  );
+}
+
+export default App;
+ 
