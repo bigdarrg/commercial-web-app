1e5279598d11af4bef37cddeaf5b1a1fc47eb810
bigdarrg <joebruce28@outlook.com>
2023-02-24 16:19:31 +0000

    Removing unused vars.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import "../src/App.css"
 
 import TabsBar from "./components/tabs-bar";
