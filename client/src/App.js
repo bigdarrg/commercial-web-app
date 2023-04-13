039960adb6ab03e8dae97cd935132206a37aaa98
bigdarrg <joebruce28@outlook.com>
2023-04-13 17:26:21 +0100

    Switched to HashRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { HashRouter as Router, Routes, Route} from "react-router-dom";
 import { Link } from 'react-router-dom';
 import "../src/App.css"
 
