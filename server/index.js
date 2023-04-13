7fb20b5e5ba5f2711753156fec49149af607c56f
bigdarrg <joebruce28@outlook.com>
2023-04-13 16:43:42 +0100

    Changed public folder name to build

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ server.use(cors());
 server.use(express.json());
 
 //Runs build placed in public dir
-server.use(express.static(path.join(__dirname + "/public")));
+server.use(express.static(path.join(__dirname + "/build")));
 
 //Opening connection to the database
 async function connectToDB() {
